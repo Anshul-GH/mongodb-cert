ec938f9f2d9bb6d42f6d72bedaa4dc5bab24ff46
Anshul <samgalacestyle01@gmail.com>
2018-06-23 15:04:59 +0530

    Changes corresponding to Bottle Framework and Pymongo exception Handling

--- bottleWebAppAdv.py
+++ bottleWebAppAdv.py
@@ -7,5 +7,22 @@ def home_page():
     return bottle.template('hello_world', username='Anshul', things=mythings)
 
 
+@bottle.post('/favourite_fruit')
+def favourite_fruit():
+    fruit = bottle.request.forms.get('fruit')
+    if fruit is None or fruit == "":
+        fruit = "No Fruit Selected"
+
+    # return bottle.template('fruit_selection.tpl', {'fruit':fruit})
+    bottle.response.set_cookie("fruit", fruit)
+    bottle.redirect("/show_fruit")
+
+
+@bottle.route('/show_fruit')
+def show_fruit():
+    fruit = bottle.request.get_cookie("fruit")
+    return bottle.template('fruit_selection.tpl', {'fruit': fruit})
+
+
 bottle.debug(True)
 bottle.run(host='localhost', port=8080)
